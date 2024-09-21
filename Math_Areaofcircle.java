e794ffea15f9795cb4dc7c92ed48df71d127196e
anilanair2012 <anilanair2012@gmail.com>
2024-09-21 09:32:40 +0530

    1

---
+++ Math_Areaofcircle.java
@@ -0,0 +1,13 @@
+package basicprogram;
+
+
+
+public class Math_Areaofcircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double r=Math.random();
+System.out.println(Math.PI*r*r);
+	
+	}
+}
